9e8eea90c913f9e67857cacd916bbdb45b16838e
SpecialAPI <pavlov.andrei.d@gmail.com>
2022-02-12 20:54:26 +0300

    Make NodeAPI work for normal version of the game + extra bugproofing.

--- Patches/MapGenerator_ChooseSpecialNodeFromPossibilities.cs
+++ Patches/MapGenerator_ChooseSpecialNodeFromPossibilities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using DiskCardGame;
 using HarmonyLib;
@@ -13,14 +14,18 @@ namespace NodeAPI.Patches
         public static void Prefix(ref List<NodeData> possibilities)
         {
             NewNode.MapNodeType type = NewNode.MapNodeType.NONE;
-            if (possibilities[0] is BuyPeltsNodeData && possibilities[1] is TradePeltsNodeData && possibilities[2] is DeckTrialNodeData && possibilities[3] is BoulderChoiceNodeData)
+            if (possibilities.Count > 3 && possibilities[0] is BuyPeltsNodeData && possibilities[1] is TradePeltsNodeData && possibilities[2] is DeckTrialNodeData && possibilities[3] is BoulderChoiceNodeData)
             {
                 type = NewNode.MapNodeType.SpecialCardChoice;
             }
-            else if(possibilities[0] is CardMergeNodeData && possibilities[1] is GainConsumablesNodeData && possibilities[2] is BuildTotemNodeData && possibilities[3] is DuplicateMergeNodeData && possibilities[4] is CardRemoveNodeData && possibilities[5] is
-                CardStatBoostNodeData && possibilities[6] is CopyCardNodeData)
+            else if (possibilities.Count > 5 && possibilities[0] is CardMergeNodeData && possibilities[1] is GainConsumablesNodeData && possibilities[2] is BuildTotemNodeData && possibilities[3] is DuplicateMergeNodeData && 
+                possibilities[4] is CardRemoveNodeData && possibilities[5] is CardStatBoostNodeData)
             {
-                type = NewNode.MapNodeType.Other;
+                Type copyCardType = typeof(CardStatBoostNodeData).Assembly.GetTypes().ToList().Find((x) => x.Name == "CopyCardNodeData");
+                if (copyCardType == null || (possibilities.Count > 6 && possibilities[6].GetType() == copyCardType))
+                {
+                    type = NewNode.MapNodeType.Other;
+                }
             }
             if(type != NewNode.MapNodeType.NONE)
             {
